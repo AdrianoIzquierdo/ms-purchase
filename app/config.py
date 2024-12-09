70388510dd603a1a79b59f35c3a3ebb734a8f11c
Adriano Izquierdo <128609907+AdrianoIzquierdo@users.noreply.github.com>
2024-12-09 16:11:16 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 import redis
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+mysqlconnector://root:password@mysql_db:3306/catalogodb')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql+psycopg2://usuario:contraseña@host:puerto/ms-purchase')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
     REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
